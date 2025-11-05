0cb655d153ea5496900d0936f053332568f60e5a
coderboy-raiyan <tajkierhaque@gmail.com>
2025-11-05 21:05:34 +0600

    feat: implement SinglyLinkedList class with append, prepend, insert, remove, and search functionalities

--- critical-thinker/module-3/index.ts
+++ critical-thinker/module-3/index.ts
@@ -1,12 +1,147 @@
-import Stack from "./stack";
+class Node<T> {
+  val: T;
+  next: Node<T> | null;
+  constructor(val: T) {
+    this.val = val;
+    this.next = null;
+  }
+}
 
-const stack = new Stack<number>();
-stack.push(20);
-stack.push(30);
-stack.push(90);
-stack.push(100);
+class SinglyLinkedList<T> {
+  tail: Node<T> | null;
+  head: Node<T> | null;
+  length: number;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
-console.log(stack.peek());
-console.log(stack.pop());
-console.log(stack.pop());
-console.log(stack.print());
+  append(val: T) {
+    this.length++;
+    if (this.head === null && this.tail === null) {
+      this.head = new Node(val);
+      this.tail = this.head;
+      return;
+    }
+    const newNode = new Node(val);
+    this.tail!.next = newNode;
+    this.tail = newNode;
+  }
+
+  prepend(val: T) {
+    this.length++;
+    if (this.head === null && this.tail === null) {
+      this.head = new Node(val);
+      this.tail = new Node(val);
+      return;
+    }
+
+    const newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  insert(val: T, index: number) {
+    if (index > this.length) {
+      console.log("Invalid position ", index);
+      return;
+    }
+
+    if (index === 1) {
+      this.prepend(val);
+      return;
+    }
+    if (index === this.length) {
+      this.append(val);
+      return;
+    }
+
+    let newNode = new Node(val);
+    let prevNode = this.findPrevNode(index);
+    newNode.next = prevNode!.next;
+    prevNode!.next = newNode;
+    this.length++;
+  }
+  removeHead() {
+    if (this.head === null) return;
+    this.head = this.head.next;
+    this.length--;
+  }
+  removeTail() {
+    if (this.tail === null) return;
+    let prevNode = this.findPrevNode(this.length - 1);
+    prevNode!.next = null;
+    this.tail = prevNode;
+    this.length--;
+  }
+
+  remove(index?: number) {
+    if (index && index > this.length) {
+      console.log("Invalid position ", index);
+      return;
+    }
+    if (index && index === 0) {
+      this.removeHead();
+      return;
+    }
+    if (index && index === this.length) {
+      this.removeTail();
+      return;
+    }
+
+    if (index) {
+      let prevNode = this.findPrevNode(index);
+      prevNode!.next = prevNode!.next!.next;
+      this.length--;
+    }
+  }
+
+  searchNode(val: T) {
+    let temp = this.head;
+    while (temp !== null) {
+      if (temp.val === val) {
+        return temp;
+      }
+      temp = temp.next;
+    }
+  }
+
+  private findPrevNode(index: number) {
+    let i = 0;
+    let temp = this.head;
+    while (temp !== null && i < index - 1) {
+      temp = temp.next;
+      i++;
+    }
+    return temp;
+  }
+
+  private lastNode() {
+    let temp = this.head;
+    while (temp !== null && temp?.next !== null) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+
+  public print() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp?.val);
+      temp = temp.next;
+    }
+  }
+}
+
+const list = new SinglyLinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.append(40);
+
+list.remove(2);
+
+list.print();
+
+console.log(`size : ${list.length}`);
